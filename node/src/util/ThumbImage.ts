da9a51a0554afb3e7b9a471a9dd0c524e829dd59
SaekiTominaga <kcsu3470+github@gmail.com>
2022-11-30 01:54:27 +0900

    #21 type を後から setter で変更したときに反映されなくなったバグを修正

--- node/src/util/ThumbImage.ts
+++ node/src/util/ThumbImage.ts
@@ -1,12 +1,12 @@
 import path from 'path';
-import { NoName2 as ImageInfos } from '../../configure/type/thumb-image.js';
+import { NoName2 as ImageInfo } from '../../configure/type/thumb-image.js';
 
 /**
  * サムネイル画像
  */
 export default class ThumbImage {
 	/* ファイルタイプ毎の MIME タイプや拡張子の定義 */
-	#imageInfo;
+	#imageInfo: ImageInfo;
 
 	/* サムネイル画像を保存するルートディレクトリ */
 	#dir: string;
@@ -24,19 +24,14 @@ export default class ThumbImage {
 	#quality: number | undefined;
 
 	/**
-	 * @param {ImageInfos} imageInfos - ファイルタイプ毎の MIME タイプや拡張子の定義
+	 * @param {ImageInfo} imageInfo - ファイルタイプ毎の MIME タイプや拡張子の定義
 	 * @param {string} dir - サムネイル画像を保存するルートディレクトリ
 	 * @param {string} fileBasePath - ベースとなるファイルパス
 	 * @param {object} type - 画像タイプ
 	 * @param {object} size - 画像サイズ
 	 * @param {number} quality - 画像品質
 	 */
-	constructor(imageInfos: ImageInfos, dir: string, fileBasePath: string, type: ImageType, size: ImageSize, quality?: number) {
-		const imageInfo = imageInfos[type];
-		if (imageInfo === undefined) {
-			throw new Error('Specified image type information is not registered');
-		}
-
+	constructor(imageInfo: ImageInfo, dir: string, fileBasePath: string, type: ImageType, size: ImageSize, quality?: number) {
 		this.#imageInfo = imageInfo;
 		this.#dir = dir;
 		this.#fileBasePath = fileBasePath;
@@ -46,17 +41,33 @@ export default class ThumbImage {
 		this.#quality = quality;
 	}
 
+	/**
+	 * 画像定義を取得する
+	 *
+	 * @returns {object} MIME タイプや拡張子の定義
+	 */
+	#getImageInfo() {
+		const imageInfo = this.#imageInfo[this.#type];
+		if (imageInfo === undefined) {
+			throw new Error('Specified image type information is not registered.');
+		}
+
+		return imageInfo;
+	}
+
 	get fileBasePath(): string {
 		return this.#fileBasePath;
 	}
 
 	get filePath(): string {
+		const imageInfo = this.#getImageInfo();
+
 		const paramSize = `s=${this.#size.width}x${this.#size.height}`;
 		const paramQuality = `q=${this.#quality}`;
 
-		const params = this.#imageInfo.quality ? [paramSize, paramQuality] : [paramSize];
+		const params = imageInfo.quality ? [paramSize, paramQuality] : [paramSize];
 
-		return `${this.#fileBasePath}@${params.join(';')}.${this.#imageInfo.extension}`; // e.g `path/to.jpg@s=100x200;q=80.webp`
+		return `${this.#fileBasePath}@${params.join(';')}.${imageInfo.extension}`; // e.g `path/to.jpg@s=100x200;q=80.webp`
 	}
 
 	get fileFullPath(): string {
@@ -64,11 +75,11 @@ export default class ThumbImage {
 	}
 
 	get mime(): string {
-		return this.#imageInfo.mime;
+		return this.#getImageInfo().mime;
 	}
 
 	get altType(): ImageType | undefined {
-		return this.#imageInfo.alt_type;
+		return this.#getImageInfo().alt_type;
 	}
 
 	get type(): ImageType {
