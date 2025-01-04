3a71e6e618ce0f7ba278f7f3eec03b62408c1158
SaekiTominaga <kcsu3470+github@gmail.com>
2025-01-04 17:12:04 +0900

    hono 化により不要となった型情報を削除

--- node/@types/blog-upload.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace BlogUploadRequest {
-	export interface Query {
-		file_name: string;
-		mime: string;
-		temp_path: string;
-		size: number;
-		overwrite: boolean;
-	}
-}
--- node/@types/thumb-image.d.ts
+++ node/@types/thumb-image.d.ts
@@ -2,23 +2,3 @@ interface ImageSize {
 	width: number;
 	height: number;
 }
-
-declare namespace ThumbImageRequest {
-	export interface Query {
-		readonly path: string;
-		readonly type: string;
-		readonly width: number | null;
-		readonly height: number | null;
-		readonly quality: number;
-	}
-}
-
-declare namespace ThumbImageCreateRequest {
-	export interface Query {
-		readonly file_path: string;
-		readonly type: string;
-		readonly width: number;
-		readonly height: number;
-		readonly quality: number;
-	}
-}
