5f1f55d525fffb0e33f0ffe718fd491b5782942c
SaekiTominaga <kcsu3470+github@gmail.com>
2025-10-25 20:14:26 +0900

    content-length の指定を削除

--- node/src/app.test.ts
+++ node/src/app.test.ts
@@ -30,12 +30,12 @@ await test('Omission of extension', async () => {
 
 await test('favicon.ico', async (t) => {
 	await t.test('no compression', async () => {
-		const [file, res] = await Promise.all([fs.promises.readFile(`${config.static.root}/favicon.svg`), app.request('/favicon.ico')]);
+		const res = await app.request('/favicon.ico');
 
 		assert.equal(res.status, 200);
 		assert.equal(res.headers.get('Content-Type'), 'image/svg+xml;charset=utf-8');
+		assert.equal(res.headers.get('Content-Encoding'), null);
 		assert.equal(res.headers.get('Cache-Control'), 'max-age=604800');
-		assert.equal(res.headers.get('Content-Length'), String(file.byteLength));
 	});
 
 	await t.test('gzip', async () => {
@@ -47,15 +47,11 @@ await test('favicon.ico', async (t) => {
 	});
 
 	await t.test('brotli', async () => {
-		const [file, res] = await Promise.all([
-			fs.promises.readFile(`${config.static.root}/favicon.svg.br`),
-			app.request('/favicon.ico', {
-				headers: { 'accept-encoding': 'gzip, deflate, br;q=1.0, zstd, *;q=0.5' },
-			}),
-		]);
+		const res = await app.request('/favicon.ico', {
+			headers: { 'accept-encoding': 'gzip, deflate, br;q=1.0, zstd, *;q=0.5' },
+		});
 
 		assert.equal(res.headers.get('Content-Encoding'), 'br');
-		assert.equal(res.headers.get('Content-Length'), String(file.byteLength));
 	});
 });
 
